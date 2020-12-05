28dbac6e2a3535139a215f761b21f85c4b026c42
spaceC00kie <kirstie317@gmail.com>
2020-12-04 23:08:27 -0500

    getting it!

---
+++ practice-04.js
@@ -0,0 +1,10 @@
+//given an array print out the itm of the array through an arrow function and use forEach
+
+const printCows = (cow) =>
+  cows.forEach((cow) => {
+    console.log(cows);
+  });
+
+const cows = ["brown", "black", "spotted"];
+
+console.log(cows);
